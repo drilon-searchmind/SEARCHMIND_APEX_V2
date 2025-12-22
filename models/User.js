480055e43102aef6140c06ab806f8108db6efa05
drili <drilib@hotmail.com>
2025-12-22 23:53:11 +0100

    :hammer: added bcrypt and jwt to user model password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -33,6 +34,13 @@ const UserSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+});
+
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 export default mongoose.models.User || mongoose.model('User', UserSchema);
