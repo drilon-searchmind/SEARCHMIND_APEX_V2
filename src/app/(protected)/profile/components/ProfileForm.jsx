d8fff8796a0293995fcf8b56f36a3a2b3df8f2e3
drilon-searchmind <dbr@searchmind.dk>
2026-01-06 15:48:23 +0100

    :bug: fixed small image error request

--- src/app/(protected)/profile/components/ProfileForm.jsx
+++ src/app/(protected)/profile/components/ProfileForm.jsx
@@ -17,7 +17,7 @@ export default function ProfileForm({ form, onChange, onSubmit, saving }) {
                 <span className="flex flex-col w-full">
                     <div className="h-16 w-16 rounded-full overflow-hidden border border-gray-200 bg-gray-50">
                         <Image
-                            src={form.image || "/images/logo.png"}
+                            src={form.image || ""}
                             alt="Avatar"
                             width={64}
                             height={64}
